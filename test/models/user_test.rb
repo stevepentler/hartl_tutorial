4eebfcf88302845c709edbf9ce55c89d1b9b1888
Steve Pentler <stevepentler@gmail.com>
2016-01-26 11:09:21 -0700

    User authentication and validation tests complete

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,10 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", 
+                     email: "user@example.com", 
+                     password: "userpassword", 
+                     password_confirmation: "userpassword")
   end
 
   test "should be valid" do
@@ -45,5 +48,22 @@ class UserTest < ActiveSupport::TestCase
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end
