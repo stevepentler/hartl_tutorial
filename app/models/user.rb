43ce163063866e8300ab9d2a6f7300eb1bd3e122
Steve Pentler <stevepentler@gmail.com>
2016-01-30 10:01:26 -0700

    10.1 finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,6 +40,17 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, nil)
   end
 
+  #activates an account 
+  def activate
+    update_attribute(:activated, true)
+    update_attribute(:activated_at, Time.zone.now)
+  end 
+
+  #sends activation email
+  def send_activation_email 
+    UserMailer.account_activation(self).deliver_now
+  end 
+
   private 
 
   def downcase_email
