ec7360b759c55ab236fd4d02051727099c38b194
Steve Pentler <stevepentler@gmail.com>
2016-01-30 09:40:21 -0700

    email sends, through 10.1.2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,9 @@
 Rails.application.configure do
+ 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'example.com'
+  config.action_mailer.default_url_options = { host: host }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -13,8 +18,6 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
